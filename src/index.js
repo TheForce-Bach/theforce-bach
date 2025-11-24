5cabcd631a3d2acf430620829b7be288dc32fb76
Aria <114254947+vamparia-purple@users.noreply.github.com>
2025-11-24 08:14:11 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import { gsap } from "gsap";
 
 ReactDOM.render(
   <React.StrictMode>
